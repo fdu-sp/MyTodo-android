f5be9160016e2e0ce662a36798513762ef5350b3
zmark <351479188@qq.com>
2023-11-26 21:43:10 +0800

    feat:新增一个浮动按钮用于新增todo项

--- app/src/main/java/com/zmark/mytodo/MainActivity.java
+++ app/src/main/java/com/zmark/mytodo/MainActivity.java
@@ -1,11 +1,15 @@
 package com.zmark.mytodo;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.zmark.mytodo.fragment.CalendarViewFragment;
 import com.zmark.mytodo.fragment.HomeFragment;
 import com.zmark.mytodo.fragment.MyDayFragment;
@@ -42,6 +46,38 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        // 获取 FloatingActionButton
+        FloatingActionButton fabAddTodo = findViewById(R.id.fab_add_todo);
+
+        // 设置点击监听器
+        fabAddTodo.setOnClickListener(view -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("新建待办事项");
+
+            // 设置对话框布局
+            View dialogView = getLayoutInflater().inflate(R.layout.dialog_add_todo, null);
+            builder.setView(dialogView);
+
+            // 获取对话框中的 EditText
+            EditText editTextTodo = dialogView.findViewById(R.id.editTextTodo);
+
+            // 设置对话框按钮
+            builder.setPositiveButton("确定", (dialog, which) -> {
+                // 处理用户点击确定按钮的逻辑
+                String todoText = editTextTodo.getText().toString();
+                // 执行添加待办事项的操作
+                // 可以将任务添加到数据库或其他数据源
+                // 更新 UI 或重新加载数据
+            });
+
+            builder.setNegativeButton("取消", (dialog, which) -> {
+                // 处理用户点击取消按钮的逻辑
+                dialog.cancel();
+            });
+
+            builder.show();
+        });
+        
         // 加载默认的Fragment
         loadFragment(new HomeFragment());
     }
