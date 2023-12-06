ea90aea714c66435d17b498e10214717b47744af
zmark <351479188@qq.com>
2023-12-06 15:42:17 +0800

    chore:依赖文件删除注释掉的依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-//    implementation(files("libs\\MyTodo-0.0.1-SNAPSHOT-jar-with-dependencies.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
