9ff03b7ed00b3990d87b426e3d344169a1136054
zmark <351479188@qq.com>
2023-12-09 00:14:55 +0800

    fix:TaskGroupService

--- app/src/main/java/com/zmark/mytodo/api/TaskGroupService.java
+++ app/src/main/java/com/zmark/mytodo/api/TaskGroupService.java
@@ -6,14 +6,15 @@ import com.zmark.mytodo.api.result.Result;
 
 import java.util.List;
 
+import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface TaskGroupService {
     @GET("/api/task-group/simple/get-all")
-    public Result<List<TaskGroupSimpleResp>> getAllTaskGroup();
+    Call<Result<List<TaskGroupSimpleResp>>> getAllTaskGroup();
 
     @POST("/api/task-group/create")
-    public Result<TaskGroupSimpleResp> createNew(@Body TaskGroupCreateReq createReq);
+    Call<Result<TaskGroupSimpleResp>> createNew(@Body TaskGroupCreateReq createReq);
 }
