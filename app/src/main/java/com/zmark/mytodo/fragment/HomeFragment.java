a4b2596a95d0a25f89bb7dd22b238a517159ce9d
zmark <351479188@qq.com>
2023-12-09 00:24:55 +0800

    feat:HomeFragment对接分组信息

--- app/src/main/java/com/zmark/mytodo/fragment/HomeFragment.java
+++ app/src/main/java/com/zmark/mytodo/fragment/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.zmark.mytodo.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,53 +13,106 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.zmark.mytodo.MainApplication;
 import com.zmark.mytodo.R;
+import com.zmark.mytodo.api.ApiUtils;
+import com.zmark.mytodo.api.TaskGroupService;
+import com.zmark.mytodo.api.bo.group.resp.TaskGroupSimpleResp;
+import com.zmark.mytodo.api.invariant.Msg;
+import com.zmark.mytodo.api.result.Result;
+import com.zmark.mytodo.api.result.ResultCode;
 import com.zmark.mytodo.model.group.TaskGroup;
 import com.zmark.mytodo.model.group.TaskGroupAdapter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class HomeFragment extends Fragment {
+    private final static String TAG = "HomeFragment";
+    private RecyclerView containerView;
+    private List<TaskGroup> taskGroups;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.taskGroups = new ArrayList<>();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        this.findView(view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Assuming you have a RecyclerView for task groups
-        RecyclerView taskGroupRecyclerView = view.findViewById(R.id.taskGroupRecyclerView);
-
-        // Assuming you have a list of TaskGroup objects
-        List<TaskGroup> taskGroups = getTaskGroups(); // Your method to get task groups
-
-        // Create a TaskGroupAdapter
-        TaskGroupAdapter taskGroupAdapter = new TaskGroupAdapter(taskGroups);
-
-        // Set the adapter to the RecyclerView
-        taskGroupRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-        taskGroupRecyclerView.setAdapter(taskGroupAdapter);
+        // todo 注册顶部菜单的点击事件
+        // 获取分组数据并更新UI
+        this.fetchDataAndUpdateUI();
     }
 
-    private List<TaskGroup> getTaskGroups() {
-        List<TaskGroup> taskGroups = new ArrayList<>();
-        taskGroups.add(new TaskGroup("专业课程", Arrays.asList("智能移动平台应用开发", "软件设计", "操作系统", "计算机网络", "机器学习", "概率论与数理统计", "数据库设计")));
-        taskGroups.add(new TaskGroup("专业实践", Arrays.asList("软件工程实践", "软件测试", "软件项目管理", "软件需求工程", "软件体系结构设计", "软件工程经济学")));
-        taskGroups.add(new TaskGroup("Group 2", Arrays.asList("List 4", "List 5", "List 6")));
-        taskGroups.add(new TaskGroup("Group 3", Arrays.asList("List 7", "List 8", "List 9")));
-        taskGroups.add(new TaskGroup("Group 4", Arrays.asList("List 10", "List 11", "List 12")));
-        taskGroups.add(new TaskGroup("Group 5", Arrays.asList("List 13", "List 14", "List 15")));
-        taskGroups.add(new TaskGroup("Group 6", Arrays.asList("List 16", "List 17", "List 18")));
-        taskGroups.add(new TaskGroup("Group 7", Arrays.asList("List 19", "List 20", "List 21")));
-        return taskGroups;
+    private void findView(View view) {
+        containerView = view.findViewById(R.id.taskGroupRecyclerView);
+    }
+
+    private void fetchDataAndUpdateUI() {
+        TaskGroupService taskGroupService = MainApplication.getTaskGroupService();
+        Call<Result<List<TaskGroupSimpleResp>>> call = taskGroupService.getAllTaskGroup();
+        call.enqueue(new Callback<Result<List<TaskGroupSimpleResp>>>() {
+            @Override
+            public void onResponse(@NonNull Call<Result<List<TaskGroupSimpleResp>>> call,
+                                   @NonNull Response<Result<List<TaskGroupSimpleResp>>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getContext(), Msg.CLIENT_REQUEST_ERROR, Toast.LENGTH_SHORT).show();
+                    ApiUtils.handleResponseError(TAG, response);
+                    return;
+                }
+                Result<List<TaskGroupSimpleResp>> result = response.body();
+                if (result == null) {
+                    Log.e(TAG, "onResponse: result is null");
+                    Toast.makeText(requireContext(), Msg.SERVER_INTERNAL_ERROR, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (result.getCode() == ResultCode.SUCCESS.getCode()) {
+                    List<TaskGroupSimpleResp> simpleRespList = result.getObject();
+                    if (simpleRespList == null) {
+                        Log.e(TAG, "onResponse: simpleRespList is null");
+                        Toast.makeText(requireContext(), Msg.SERVER_INTERNAL_ERROR, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    taskGroups.clear();
+                    taskGroups.addAll(TaskGroup.from(simpleRespList));
+                    updateUI();
+                } else {
+                    Log.w(TAG, "code:" + result.getCode() + " onResponse: " + result.getMsg());
+                    Toast.makeText(requireContext(), result.getMsg(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Result<List<TaskGroupSimpleResp>>> call, @NonNull Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+                Toast.makeText(requireContext(), Msg.CLIENT_REQUEST_ERROR, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUI() {
+        requireActivity().runOnUiThread(() -> {
+            try {
+                TaskGroupAdapter taskGroupAdapter = new TaskGroupAdapter(taskGroups);
+                containerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+                containerView.setAdapter(taskGroupAdapter);
+            } catch (Exception e) {
+                Log.e(TAG, "updateUI: " + e.getMessage(), e);
+                Toast.makeText(requireContext(), Msg.CLIENT_INTERNAL_ERROR, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
