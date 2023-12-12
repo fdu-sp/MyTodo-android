015ee37d4d7e8897eca435eceee5e8ccf5db676a
zmark <351479188@qq.com>
2023-12-12 23:56:21 +0800

    fix:TaskTimeInfoResp日期和时间使用String

--- app/src/main/java/com/zmark/mytodo/service/bo/task/resp/inner/TaskTimeInfoResp.java
+++ app/src/main/java/com/zmark/mytodo/service/bo/task/resp/inner/TaskTimeInfoResp.java
@@ -1,18 +1,15 @@
 package com.zmark.mytodo.service.bo.task.resp.inner;
 
 
-import java.sql.Date;
-import java.sql.Time;
-
 /**
  * @author ZMark
  * @date 2023/12/4 10:18
  */
 public class TaskTimeInfoResp {
-    private Date endDate;
-    private Time endTime;
+    private String endDate;
+    private String endTime;
     private Boolean activateCountdown;
-    private Date expectedExecutionDate;
-    private Time expectedExecutionStartPeriod;
-    private Time expectedExecutionEndPeriod;
+    private String expectedExecutionDate;
+    private String expectedExecutionStartPeriod;
+    private String expectedExecutionEndPeriod;
 }
