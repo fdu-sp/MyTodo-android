428b48a6af73e320d3ee5743a3ba7841d6d455b4
zmark <351479188@qq.com>
2023-12-13 21:02:58 +0800

    feat:新增getter和setter方法

--- app/src/main/java/com/zmark/mytodo/service/bo/task/resp/inner/TaskContentInfoResp.java
+++ app/src/main/java/com/zmark/mytodo/service/bo/task/resp/inner/TaskContentInfoResp.java
@@ -15,4 +15,28 @@ public class TaskContentInfoResp {
         this.createTime = null;
         this.updateTime = null;
     }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
 }
