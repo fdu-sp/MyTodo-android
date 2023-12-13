e56daf4e30d4cef24a4f2bb7a1476d3e7c0ee38b
zmark <351479188@qq.com>
2023-12-14 00:42:35 +0800

    feat:新增TagSimple

---
+++ app/src/main/java/com/zmark/mytodo/model/tag/TagSimple.java
@@ -0,0 +1,39 @@
+package com.zmark.mytodo.model.tag;
+
+import com.zmark.mytodo.service.bo.tag.resp.TagSimpleResp;
+
+public class TagSimple {
+    private Long id;
+    private String tagName;
+    private String tagPath;
+
+    public TagSimple(TagSimpleResp tagSimpleResp) {
+        this.id = tagSimpleResp.getId();
+        this.tagName = tagSimpleResp.getTagName();
+        this.tagPath = tagSimpleResp.getTagPath();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public void setTagName(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public String getTagPath() {
+        return tagPath;
+    }
+
+    public void setTagPath(String tagPath) {
+        this.tagPath = tagPath;
+    }
+}
