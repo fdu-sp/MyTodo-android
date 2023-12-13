8a9e5c7ce2785ab59f06aa03a3523b41b5923407
zmark <351479188@qq.com>
2023-12-14 01:07:59 +0800

    feat:新增TagSelectBottomSheetFragment

---
+++ app/src/main/java/com/zmark/mytodo/fragment/common/TagSelectBottomSheetFragment.java
@@ -0,0 +1,113 @@
+package com.zmark.mytodo.fragment.common;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.zmark.mytodo.MainApplication;
+import com.zmark.mytodo.R;
+import com.zmark.mytodo.model.tag.TagSimple;
+import com.zmark.mytodo.model.tag.TagSimpleListAdapter;
+import com.zmark.mytodo.service.ApiUtils;
+import com.zmark.mytodo.service.bo.tag.resp.TagSimpleResp;
+import com.zmark.mytodo.service.invariant.Msg;
+import com.zmark.mytodo.service.result.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+
+public class TagSelectBottomSheetFragment extends BottomSheetDialogFragment {
+
+    private final static String TAG = "TagSelectBottomSheetFragment";
+
+
+    /**
+     * data-Tag列表
+     */
+    private List<TagSimple> tagList;
+
+    private TagSimpleListAdapter.OnTagSelectListener onTagSelectListener;
+
+    private RecyclerView tagSimpleListRecyclerView;
+
+    public void setOnTagSelectListener(TagSimpleListAdapter.OnTagSelectListener listener) {
+        this.onTagSelectListener = listener;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.tagList = new ArrayList<>();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tag_simple_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tagSimpleListRecyclerView = view.findViewById(R.id.tagSimpleListRecyclerView);
+        // 获取数据并更新UI
+        this.fetchDataAndUpdateUI();
+    }
+
+    private void fetchDataAndUpdateUI() {
+        Call<Result<List<TagSimpleResp>>> call = MainApplication.getTagService().getAllTagsSimple();
+
+        ApiUtils.doRequest(call, new ApiUtils.Callbacks<List<TagSimpleResp>>() {
+
+            @Override
+            public void onSuccess(List<TagSimpleResp> data) {
+                for (TagSimpleResp tagSimpleResp : data) {
+                    tagList.add(new TagSimple(tagSimpleResp));
+                }
+                updateUI();
+            }
+
+            @Override
+            public void onFailure(Integer code, String msg) {
+                Log.w(TAG, "onFailure: " + code + " " + msg);
+                Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onClientRequestError(Throwable t) {
+                Log.e(TAG, "onFailure: ", t);
+                Toast.makeText(requireContext(), Msg.CLIENT_REQUEST_ERROR, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onServerInternalError() {
+                Toast.makeText(requireContext(), Msg.SERVER_INTERNAL_ERROR, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void updateUI() {
+        requireActivity().runOnUiThread(() -> {
+            TagSimpleListAdapter adapter = new TagSimpleListAdapter(tagList);
+            tagSimpleListRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
+            adapter.setOnTagSelectListener(tagSimple -> {
+                if (onTagSelectListener != null) {
+                    onTagSelectListener.onTagSelect(tagSimple);
+                }
+                dismiss();
+            });
+            tagSimpleListRecyclerView.setAdapter(adapter);
+
+        });
+    }
+}
---
+++ app/src/main/java/com/zmark/mytodo/model/tag/TagSimpleListAdapter.java
@@ -0,0 +1,74 @@
+package com.zmark.mytodo.model.tag;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zmark.mytodo.R;
+
+import java.util.List;
+
+public class TagSimpleListAdapter extends RecyclerView.Adapter<TagSimpleListAdapter.ViewHolder> {
+    public interface OnTagSelectListener {
+        void onTagSelect(TagSimple tagSimple);
+    }
+
+    private List<TagSimple> tagList;
+    private OnTagSelectListener onTagSelectListener;
+
+    public TagSimpleListAdapter(List<TagSimple> tagList) {
+        this.tagList = tagList;
+    }
+
+    public void setOnTagSelectListener(OnTagSelectListener listener) {
+        this.onTagSelectListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_tag_simple, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        TagSimple tag = tagList.get(position);
+        String tagPath = tag.getTagPath();
+        if (tagPath.split("/").length == 1) {
+            holder.imageViewTagIcon.setImageResource(R.drawable.ic_tag_1_simple);
+        } else {
+            holder.imageViewTagIcon.setImageResource(R.drawable.ic_tag_2_no_simple);
+        }
+        holder.tagLinearLayout.setOnClickListener(v -> {
+            if (onTagSelectListener != null) {
+                onTagSelectListener.onTagSelect(tag);
+            }
+        });
+        holder.textViewTagPath.setText(tag.getTagPath());
+    }
+
+    @Override
+    public int getItemCount() {
+        return tagList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout tagLinearLayout;
+        ImageView imageViewTagIcon;
+        TextView textViewTagPath;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tagLinearLayout = itemView.findViewById(R.id.tagLinearLayout);
+            imageViewTagIcon = itemView.findViewById(R.id.imageViewTagIcon);
+            textViewTagPath = itemView.findViewById(R.id.textViewTagPath);
+        }
+    }
+}
