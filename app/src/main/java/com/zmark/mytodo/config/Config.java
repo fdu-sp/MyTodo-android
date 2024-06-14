4be1a9a44606f962a2bd7b39e8359f54419f020d
zmark <351479188@qq.com>
2024-06-14 20:35:42 +0800

    chore: 使用服务器ip和端口

--- app/src/main/java/com/zmark/mytodo/config/Config.java
+++ app/src/main/java/com/zmark/mytodo/config/Config.java
@@ -3,11 +3,12 @@ package com.zmark.mytodo.config;
 public class Config {
     private static final String AGREEMENT = "http://";
 
-    private static final String SERVER_IP = "10.117.65.255";
+    //    private static final String SERVER_IP = "10.117.65.255";
 //    private static final String SERVER_IP = "192.168.31.61";
+    private static final String SERVER_IP = "101.37.91.15";
 
-    private static final String SERVER_PORT = "8788"; // prod
-//    private static final String SERVER_PORT = "8787"; // dev
+    //    private static final String SERVER_PORT = "8788"; // prod
+    private static final String SERVER_PORT = "8787";
 
     /**
      * baseUrl must end in /
